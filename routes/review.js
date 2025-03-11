b30e438a0b3ee33de8e52e444b3abc98ca2e6a76
KeshavVerma-Programmer <keshav.dev.code@gmail.com>
2025-03-11 12:53:12 +0530

    commiting

--- routes/review.js
+++ routes/review.js
@@ -4,7 +4,7 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const Listing = require("../models/listing.js");
 const Review = require('../models/review.js');
-const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor, isOwner } = require("../middleware.js");
 
 
 
